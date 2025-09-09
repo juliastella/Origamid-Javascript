119c38e0f45958159ee00b3cd09293bdd4aa1080
C19322Q <71910660+juliastella@users.noreply.github.com>
2025-09-09 15:02:42 -0300

    Inicio
    Aula 2 de operadores

---
+++ iniciante/numeros_operadores.js
@@ -0,0 +1,40 @@
+// operadores aritmémticos Unários
+
+var incremento = 5;
+console.log(incremento++); // 5, porque retorna o valor anterior, ou seja 5
+console.log(incremento); // 6
+
+
+var incremento2 = 5;
+console.log(++incremento2); // 6        
+console.log(incremento2); // 6
+
+var frase = 'isso é um teste';
++frase; // NaN
+-frase;
+
+var idade = '28';
++idade; // 28 (número)
+-idade // -28
+
+console.log(+idade + 5); // 33
+
+var possuiFaculdade = true;
+console.log(+ProcessingInstruction); // 1
+
+// execicio
+
+var total = 10 + 5 * 2 / 2 + 20;
+
+console.log(total);
+
+var span = 'toda a ideia é falsa';
+
+-span
+console.log(span);
+
+
+var somaString = '200';
+var somaString2 = 50;
+console.log(+somaString + somaString2); //250
+
