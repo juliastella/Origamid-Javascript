4e57089f76b989374642faec15c268f66e9b1ce2
C19322Q <julia.depaula@experian.com>
2025-11-13 17:17:58 -0300

    string

---
+++ Objetos/string/script.js
@@ -0,0 +1,29 @@
+/**
+ * Strind
+ * 
+ * É a construtora de strings, toda string possui as propriedades 
+ * e métodos do prototype de String
+ * 
+*/
+
+const comida = 'Pizza';
+const liquido = new String('Água');
+const ano = new String(2018);
+
+
+/* Metodos 
+length =  quantidade de caracteres
+charat() = retorna o index do caracter na string
+concat() = junta duas ou mais strings
+includes(search, position) = procura a string exata dentro da outra stirng
+endsWith() = procura pela string exata dentro de outra string no final 
+startsWith = procura pela string exata dentro de outra string no começo
+slice(start, end) = corta a string 
+substring(start, end) = a mesma coisa do slice, porém não aceita valores negativos de indece
+indexof(search) = retorna o index do caractere na string, ele pega o primeiro que aparece
+lastindexof(search) =  mesma coisa do indexof, contudo é ao final 
+padStart(n, str) = Adiciona caracteres no início da string até atingir o comprimento desejado
+padEnd(n, str) = Adiciona caracteres no final da string até atingir o comprimento desejado
+repeat(n) = repete a string
+replace(regexp|substr, nerstr|function) = 
+*/
