5728751ea2592b49e770fd3d71f529b12a2aa5f2
C19322Q <julia.depaula@experian.com>
2026-01-06 12:37:27 -0300

    erro corrigido

--- DOM2/modal/js/script.js
+++ DOM2/modal/js/script.js
@@ -3,7 +3,6 @@ import initAnimacaoScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
-import initModal from './modules/modal.js';
 
 initScrollSuave();
 initAnimacaoScroll();
